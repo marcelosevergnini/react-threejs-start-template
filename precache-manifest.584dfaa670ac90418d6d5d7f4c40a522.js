ea71eb0d449c7093a22812491f798d0c6f139536
Marcelo Severgnini <marcelo.severgnini@diconium.com>
2020-02-24 07:45:54 +0000

    Updates

---
+++ precache-manifest.584dfaa670ac90418d6d5d7f4c40a522.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e9fbf4234b16a47e547817c25e5263",
+    "url": "/react-threejs-start-template/index.html"
+  },
+  {
+    "revision": "738b374e92ad3317d6cf",
+    "url": "/react-threejs-start-template/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "8625cc18dbd12cba5f49",
+    "url": "/react-threejs-start-template/static/css/main.0af865a1.chunk.css"
+  },
+  {
+    "revision": "738b374e92ad3317d6cf",
+    "url": "/react-threejs-start-template/static/js/2.672fff12.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-threejs-start-template/static/js/2.672fff12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8625cc18dbd12cba5f49",
+    "url": "/react-threejs-start-template/static/js/main.c7e231ab.chunk.js"
+  },
+  {
+    "revision": "ffe83f088b4fabb6677f",
+    "url": "/react-threejs-start-template/static/js/runtime-main.dc757676.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-threejs-start-template/static/media/logo.5d5d9eef.svg"
+  }
+]);
