910ed1358809be696775629f55a85ee5fefbbb12
Marcelo Severgnini <marcelo.severgnini@diconium.com>
2020-02-24 07:42:59 +0000

    Updates

---
+++ precache-manifest.d157a4149e33909badaafc9853fc9a35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc9d443b9c7dcf5e5bd634c04296a28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "738b374e92ad3317d6cf",
+    "url": "/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "15b9d94b6c3fdcec554e",
+    "url": "/static/css/main.0af865a1.chunk.css"
+  },
+  {
+    "revision": "738b374e92ad3317d6cf",
+    "url": "/static/js/2.672fff12.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.672fff12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b9d94b6c3fdcec554e",
+    "url": "/static/js/main.b7cf6bd5.chunk.js"
+  },
+  {
+    "revision": "20f78a13b68bb6b6822b",
+    "url": "/static/js/runtime-main.12e2f8c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
