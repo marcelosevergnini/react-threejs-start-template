6382c056fc01104ed94a4f443777914b89713a91
Marcelo Severgnini <marcelo.severgnini@diconium.com>
2020-03-02 15:54:26 +0000

    Remove default example

--- app/components/Footer/Wrapper.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Wrapper = styled.footer`
-  display: flex;
-  justify-content: space-between;
-  padding: 3em 0;
-`;
-
-export default Wrapper;
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,43 +1,28 @@
 /**
  *
- * App
+ * App.js
  *
  * This component is the skeleton around the actual pages, and should only
  * contain code that should be seen on all pages. (e.g. navigation bar)
+ *
  */
 
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
-import GlobalStyle from '../../global-styles';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
-import FeaturePage from '../FeaturePage';
-import HomePage from '../HomePage';
-import NotFoundPage from '../NotFoundPage';
+import HomePage from 'containers/HomePage/Loadable';
+import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
-const AppWrapper = styled.div`
-  // max-width: calc(768px + 16px * 2);
-  margin: 0 auto;
-  display: flex;
-  min-height: 100%;
-  padding: 0 16px;
-  flex-direction: column;
-`;
+import GlobalStyle from '../../global-styles';
 
 export default function App() {
   return (
-    <AppWrapper>
-      <Header />
+    <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
-        <Route path="" component={NotFoundPage} />
+        <Route component={NotFoundPage} />
       </Switch>
-      <Footer />
       <GlobalStyle />
-    </AppWrapper>
+    </div>
   );
 }
