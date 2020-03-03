46640700b24477fbdef71c11a453eb9c31c892e4
Marcelo Severgnini <marcelo.severgnini@diconium.com>
2020-03-03 09:36:28 +0000

    [feat]: Call to Engine component

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,13 +6,22 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import {Engine} from "../../components/Engine/Engine";
+import Header from "../../components/Header";
 
 export default function HomePage() {
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <div>
+      <Header />
+      <div className="container is-fluid content-spaces ">
+        <div className="box box-color">
+          <Engine
+            backgroundColor={0xf2f2f2}
+            lights={[{ color: 0xffffff, position: { x: 1, y: 0.75, z: 0.5 } }]}
+            grid={{ size: 30000, dimensions: 60 }}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
