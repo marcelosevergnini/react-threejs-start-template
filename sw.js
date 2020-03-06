b7ca33888c7a9cd5995cc13249b99ff7cfe24f30
Marcelo Severgnini <marcelo.severgnini@diconium.com>
2020-03-06 23:08:48 +0000

    Updates

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@ var __wpo = {
   "assets": {
     "main": [
       "/favicon.ico",
-      "/runtime.3ac2b9e9840000707b55.js",
+      "/runtime.ea4ed88707f90c543de4.js",
       "/"
     ],
     "additional": [
       "/npm.intl.bd8f19d9c7d9ab22c1c5.chunk.js",
-      "/main.666fcbc1a98790a8a97e.chunk.js",
+      "/main.eb13523ff465c49e305c.chunk.js",
       "/npm.babel.d5d467dd054552ecf171.chunk.js",
       "/npm.connected-react-router.1982f00a8c9996b39191.chunk.js",
       "/npm.emotion.9178861a3319556487b8.chunk.js",
@@ -21,7 +21,7 @@ var __wpo = {
       "/npm.setimmediate.15d56feae6e28bc4cdab.chunk.js",
       "/npm.three.f9a258a36654a0de48ac.chunk.js",
       "/npm.three-orbitcontrols.107a8a9c0b28b066dae2.chunk.js",
-      "/16.aea3aff31fc4be5213bb.chunk.js",
+      "/16.d36ba1c1cce2ec778ed9.chunk.js",
       "/17.8cf3e4aeccc335ad2dfd.chunk.js",
       "/18.b7493a252e5054a14a3c.chunk.js"
     ],
@@ -31,7 +31,7 @@ var __wpo = {
   "hashesMap": {
     "5e53ef208f3fa79a48f8374488f473c76e1233de": "/favicon.ico",
     "1e7fd4463861aeb6a378cf7fb56273f34f47d204": "/npm.intl.bd8f19d9c7d9ab22c1c5.chunk.js",
-    "bacc01d540f58d2d0468292780fa13e004c6a478": "/main.666fcbc1a98790a8a97e.chunk.js",
+    "308459da7ca6771f3c912019b21edcb5a160d868": "/main.eb13523ff465c49e305c.chunk.js",
     "cb8d91f0839aa728b6b311b10efd83ca5029f0b1": "/npm.babel.d5d467dd054552ecf171.chunk.js",
     "01a18404d9d8c15a8e2fe1fe984e137663b2812a": "/npm.connected-react-router.1982f00a8c9996b39191.chunk.js",
     "c96ec75668fe511f98c10d230d7a95509078d889": "/npm.emotion.9178861a3319556487b8.chunk.js",
@@ -45,15 +45,15 @@ var __wpo = {
     "8ee62948357bbaec4066068781a7e54e7ef2e22d": "/npm.setimmediate.15d56feae6e28bc4cdab.chunk.js",
     "a62b20e1a35aa84fed622e1301376705802fc9bf": "/npm.three.f9a258a36654a0de48ac.chunk.js",
     "b86b3af8a5c575e376786f7957f774e20d0a3b53": "/npm.three-orbitcontrols.107a8a9c0b28b066dae2.chunk.js",
-    "ecadcd603e09c27d95df02f175b6a3efac5da231": "/runtime.3ac2b9e9840000707b55.js",
-    "5238e62b981932c7943932897db388b8d4d8de25": "/16.aea3aff31fc4be5213bb.chunk.js",
+    "0f91727e5f55e6b9734667ae74577a5813f1e3f7": "/runtime.ea4ed88707f90c543de4.js",
+    "04ce3748fb4b401c0cd22a5be9fe48c97dd29ac7": "/16.d36ba1c1cce2ec778ed9.chunk.js",
     "3b59f63302ba7f6e4c13e36b52319ec4b1a5d6cd": "/17.8cf3e4aeccc335ad2dfd.chunk.js",
     "1dee67f1c16ef55a66548aae426a4a91b5a3ce61": "/18.b7493a252e5054a14a3c.chunk.js",
-    "091233be25602ba21519801c016e05fafefa3562": "/"
+    "78676eb3725d98a3751da7bec663670840acfc85": "/"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "3/4/2020, 6:59:11 AM",
+  "version": "3/6/2020, 11:08:37 PM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
